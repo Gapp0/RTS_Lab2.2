67b50b0cf2f60af89f0d1afc9528524d8db03609
Gapp0 <48478241+Gapp0@users.noreply.github.com>
2020-05-25 23:38:19 +0300

    Update lab22.py

--- lab22.py
+++ lab22.py
@@ -2,9 +2,13 @@
 # #  n w    N
 # 05 14 2000 256
 
+# Додаткове завдання: реалізувати обчислення "половинок" у потоках та порівняти час виконання
+
 import random as r
 import math
 import matplotlib.pyplot as plt
+import datetime
+import concurrent.futures
 
 n = 14
 w_max = 2000
@@ -45,8 +49,39 @@ def fft(x: list):
     return fftt
 
 
+def fft_I(x: list):
+    return fft(x[:len(x)//2])
+
+
+def fft_II(x: list):
+    return fft(x[len(x)//2:])
+
+
+def parall():
+    X = graph()
+    with concurrent.futures.ThreadPoolExecutor() as exec:
+        fft_one = exec.submit(fft_I, X)
+        fft_two = exec.submit(fft_II, X)
+        result_I = fft_one.result()
+        result_II = fft_two.result()
+        return result_I, result_II
+
+
+single_t = datetime.datetime.now()
+
 X = graph()
 fftt = fft(X)
+
+single_t = datetime.datetime.now() - single_t
+
+double_t = datetime.datetime.now()
+
+parall()
+
+double_t = datetime.datetime.now() - double_t
+
+print('Single:', single_t, ', double:', double_t)
+exit(0)
 
 data_fft = [math.sqrt(fftt[i][0] ** 2 + fftt[i][1] ** 2) for i in range(N)]
 
